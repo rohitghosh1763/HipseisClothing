a0ce86f2ef6cb56ae72ef2b76757b6cf3d41cc31
Rohit Ghosh <rohitmca23-24@aec.ac.in>
2025-03-25 00:52:17 +0530

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
 const App = () => {
-    return <div>App</div>;
+    return <div>testing</div>;
 };
 
 export default App;
